7f23ba2a6035cb0e5c264dcf5e73bf64b1f55904
snipeet03 <navneetlonare77@gmail.com>
2025-11-07 21:31:10 +0530

    Refactor URL routing and update settings for media and static files

--- machine_data/urls.py
+++ machine_data/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
 
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('inventory.urls')),  
+    path('', include('machine_data.urls')),  
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT); 
